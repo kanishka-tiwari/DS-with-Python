68364340cc2cecc22ed7e951f9174cf4f6dd2de0
Kanishka Tiwari <kanishka.tiwari.sw@gmail.com>
2026-06-11 07:48:39 +0530

    Add files via upload

---
+++ 03) File, Tuple and Set/file tuple and set.py
@@ -0,0 +1,17 @@
+#if user chooses List (illustration)
+
+if structure == "List":
+    result = list(parsed)
+    print(f"Demonstration of List: {result}")
+
+#if user chooses Tuple (illustration)
+
+elif structure == "Tuple":
+    result = tuple(parsed)
+    print(f"Demonstration of Tuple: {result}")
+
+#if user chooses Set (illustration)
+
+elif structure == "Set":
+    result = set(parsed)
+    print(f"Demonstration of Set: {result}")
