646dcb3162785d6d5300a950e6b355c2a0bdc87c
Kanishka Tiwari <kanishka.tiwari.sw@gmail.com>
2026-06-11 07:49:34 +0530

    Add files via upload

---
+++ 04) Linked List/linked list.py
@@ -0,0 +1,9 @@
+#if user chooses Linked List (illustration)
+
+elif structure == "Linked List":
+    ll = linkedlist()
+    for ll in parsed:
+        ll.append(parsed)
+    print(f"Demonstation of LinkedList: {ll.display()}")
+
+#note: the blueprint must be declared before running the syntax
